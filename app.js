2e376b8570c45ae2bca25e2c2c7f59022729f0f9
Atharva Mayekar <atharvamayekar4@gmail.com>
2024-07-20 15:45:18 -0400

    Started project

---
+++ app.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+
+app.set("views", path.resolve(__dirname, 'templates'))
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get("/", (req, res) => {
+    res.render("welcome")
+})
+
+process.stdin.setEncoding("utf8");
+
+if (process.argv.length != 3) {
+    console.error("Usage app.js portNumber")
+    process.exit(1)
+}
+const portNumber = process.argv[2]
+app.listen(portNumber);
+console.log(`Web server started and running at http://localhost:${portNumber}`);
+
+process.stdin.write('Stop to shutdown the server: ')
+process.stdin.on("readable", () => {
+    const input = process.stdin.read();
+    if (input !== null) {
+        const command = input.trim();
+        if (command === "stop") {
+            process.stdout.write("Shutting down the server\n");
+            process.exit(0);
+        }
+    }
+    process.stdin.write('Stop to shutdown the server: ')
+    process.stdin.resume()
+});
