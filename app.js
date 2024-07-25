49e5670096a989533538835d361d1b368fdfcc4f
Atharva Mayekar <atharvamayekar4@gmail.com>
2024-07-24 20:03:02 -0400

    Implemented MongoDB and login features

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const path = require('path')
 const express = require('express')
+const session = require("express-session");
 const bodyParser = require('body-parser')
-const app = express();
+const app = express()
 
 app.set("views", path.resolve(__dirname, 'templates'))
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended:false}))
+
+require("dotenv").config({path: path.resolve(__dirname, ".env")})
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = process.env.MONGO_CONNECTION_STRING
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  })
+
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET}))
 
 app.get("/", (req, res) => {
     res.render("welcome")
@@ -13,6 +30,26 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.render("login")
+})
+
+app.post("/login", async (req, res) => {
+    try {
+        await client.connect()
+        let {username, password, confirm_password} = req.body
+        const result = await client.db(process.env.LOGIN_DB).collection(process.env.USER_PASS_COL).findOne({username: username})
+        if (result) {
+            res.redirect("/create_account?issue=user")
+        } else if (password == confirm_password) {
+            res.redirect("/create_account?issue=pass")
+        } else {
+            await client.db(process.env.LOGIN_DB).collection(process.env.USER_PASS_COL).insertOne({username: username, password: password})
+            res.redirect("/login")
+        }
+    } catch(e) {
+        console.error(e)
+    } finally {
+        await client.close()
+    }
 })
 
 app.get("/create_account", (req, res) => {
