8c5a66f70eae4d8088fc69db97cfb17babeb3f4d
Atharva Mayekar <atharvamayekar4@gmail.com>
2024-11-20 17:39:43 -0500

    Removed manual runner

--- app.js
+++ app.js
@@ -378,26 +378,26 @@ async function renderWatched(username) {
     }
 }
 
-process.stdin.setEncoding("utf8");
+// process.stdin.setEncoding("utf8");
 
-if (process.argv.length != 3) {
-    console.error("Usage app.js portNumber")
-    process.exit(1)
-}
-const portNumber = process.argv[2]
-app.listen(portNumber);
-console.log(`Web server started and running at http://localhost:${portNumber}`);
+// if (process.argv.length != 3) {
+//     console.error("Usage app.js portNumber")
+//     process.exit(1)
+// }
+// const portNumber = process.argv[2]
+// app.listen(portNumber);
+// console.log(`Web server started and running at http://localhost:${portNumber}`);
 
-process.stdin.write('Stop to shutdown the server: ')
-process.stdin.on("readable", () => {
-    const input = process.stdin.read();
-    if (input !== null) {
-        const command = input.trim();
-        if (command === "stop") {
-            process.stdout.write("Shutting down the server\n");
-            process.exit(0);
-        }
-    }
-    process.stdin.write('Stop to shutdown the server: ')
-    process.stdin.resume()
-});
+// process.stdin.write('Stop to shutdown the server: ')
+// process.stdin.on("readable", () => {
+//     const input = process.stdin.read();
+//     if (input !== null) {
+//         const command = input.trim();
+//         if (command === "stop") {
+//             process.stdout.write("Shutting down the server\n");
+//             process.exit(0);
+//         }
+//     }
+//     process.stdin.write('Stop to shutdown the server: ')
+//     process.stdin.resume()
+// });
